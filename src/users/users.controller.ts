9b898f5a38d4238af97a0a758053a07d18d6c9cf
Haroune Belhachani <2471001@cegepmv.ca>
2026-02-18 17:05:17 -0500

    commit initial

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDTO } from './dtos/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor(private userService : UsersService){}
+
+    @Post("/create")
+    createUser(@Body() body : CreateUserDTO){
+        this.userService.insertUser(body)
+        console.log(body)
+    }
+
+    @Get("/getAll")
+    async getAllUsers(){
+        return await this.userService.findAll()
+    }
+
+
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { CreateUserDTO } from './dtos/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor(
+        @InjectRepository(User)
+        private usersRepository: Repository<User>,
+    ) { }
+
+    async findAll() {
+        return await this.usersRepository.find()
+    }
+
+    async insertUser(userDTO: CreateUserDTO) {
+
+        /* const user = this.usersRepository.create({ email: userDTO.email, password: userDTO.password })
+
+        await this.usersRepository.insert(user) */
+
+        await this.usersRepository.save(userDTO)
+
+    }
+
+}
