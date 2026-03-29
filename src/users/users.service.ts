ee077f22e0d6240d4fc83aa24405ff64957bc609
HBelhachani <2471001@cegepmv.ca>
2026-03-29 16:34:17 -0400

    code illisible

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,8 +3,6 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { CreateUserDTO } from './dtos/create-user.dto';
-import { NotFoundError } from 'rxjs';
-import e from 'express';
 
 @Injectable()
 export class UsersService {
@@ -18,32 +16,23 @@ export class UsersService {
         return await this.usersRepository.find()
     }
 
-    createUser(userDTO: CreateUserDTO) {
-
+    async createUser(userDTO: CreateUserDTO) {
         const user = this.usersRepository.create({ email: userDTO.email, password: userDTO.password })
-
-        this.usersRepository.save(user)
-
+        await this.usersRepository.save(user)
         return user
-
-        //await this.usersRepository.save(userDTO)
-
     }
 
     async findByEmail(email: string) {
-
         const user = await this.usersRepository.findOneBy({ email: email })
-
         return user
-
     }
 
     async findOneUser(id: number) {
-
-        const user = await this.usersRepository.findOne({ where: { id: id } })
-
-        return user ? user : new NotFoundException(`Utilisateur avec id ${id} est inexistant`)
-
+        const user = await this.usersRepository.findOne({ where: { id } })
+        if (!user) {
+            throw new NotFoundException(`Utilisateur avec id ${id} est inexistant`)
+        }
+        return user
     }
 
     async deleteOneUser(id: number) {
