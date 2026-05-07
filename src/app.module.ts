fbb63e8accd33d48a67b15d6ea03e7fcc97bad76
Haroune Belhachani <2471001@cegepmv.ca>
2026-05-07 09:09:30 -0400

    update appmodule + compose.yaml

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,20 +8,28 @@ import { ProgrammableModule } from './programmable/programmable.module';
 import { InvitationModule } from './invitation/invitation.module';
 import { CalendrierModule } from './calendrier/calendrier.module';
 import { OmnivoxModule } from './omnivox/omnivox.module';
-import { ConfigModule } from '@nestjs/config';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports:
     [
-      ConfigModule.forRoot({isGlobal: true}), //env accessible everywhere
-      UsersModule,
-      TypeOrmModule.forRoot({
-        type: 'sqlite',
-        database: 'db.sqlite',
-        autoLoadEntities: true,
-        synchronize: true
+      ConfigModule.forRoot({ isGlobal: true }),
+
+      TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        useFactory: (configService: ConfigService) => ({
+          type: 'mysql',
+          host: configService.get('HOST'),
+          port: +configService.get('PORT'),
+          username: configService.get('USERNAME'),
+          password: configService.get('PASSWORD'),
+          database: configService.get('DATABASE'),
+          entities: [],
+          synchronize: true,
+        }),
+        inject: [ConfigService],
       }),
+      UsersModule,
       ProgrammableModule,
       InvitationModule,
       AuthModule,
